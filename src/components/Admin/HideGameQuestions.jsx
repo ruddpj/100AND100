4a840b714d1e18a38cee21c40c874bd0d57d1676
Joshua Cold <joshua.cold@securitymetrics.com>
2025-03-12 20:05:27 -0600

    feat(game): show/hide game questions button is disabled on final round

--- src/components/Admin/HideGameQuestions.jsx
+++ src/components/Admin/HideGameQuestions.jsx
@@ -6,17 +6,19 @@ function HideGameQuestions({ game, setGame, send }) {
   let textColor = game.settings.hide_questions ? "text-foreground" : "text-foreground";
   let buttonColor = game.settings.hide_questions ? "bg-secondary-500" : "bg-secondary-300";
   let textContent = game.settings.hide_questions ? t("Show questions") : t("Hide questions");
+  let disabledOpacity = game.is_final_round ? "opacity-50" : "";
 
   return (
     <button
       id="hideQuestionsInput"
-      className={`border-4 text-2xl ${textColor} ${buttonColor} rounded p-10`}
+      className={`rounded border-4 p-10 text-2xl ${textColor} ${buttonColor} ${disabledOpacity}`}
       onClick={() => {
         game.settings.hide_questions = !game.settings.hide_questions;
         setGame((prv) => ({ ...prv }));
         send({ action: "data", data: game });
       }}
       type="checkbox"
+      disabled={game.is_final_round}
     >
       {textContent}
     </button>
