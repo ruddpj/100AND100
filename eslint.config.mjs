bfd52c76b506aec48438b198ec2c4f19d3534698
Joshua Cold <joshua.cold@securitymetrics.com>
2025-11-13 19:03:01 -0700

    feat: setup eslint for npm run lint

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  [eslint.configs.recommended, tseslint.configs.recommended],
+  globalIgnores(["node_modules", ".next", "public", ".husky", "scripts", "tailwind.config.js"])
+);
